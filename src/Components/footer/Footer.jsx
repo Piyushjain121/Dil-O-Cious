57c306b9faf0347ae2a750ad52e51a0f04750b67
Piyushjain121 <22bcs16470@cuchd.in>
2025-12-14 22:16:07 +0530

    Update Footer.jsx

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -35,15 +35,15 @@ const Footer = () => {
                 <div id='footerDetails'>
                     <div className="contact">
                         <h1>Contact</h1>
-                        <p><i className="fa-solid fa-square-phone"></i> <span>+91-6378231364</span></p>
-                        <p><i className="fa-solid fa-envelope"></i> <span>ayushhgupta683@gmail.com</span></p>
+                        <p><i className="fa-solid fa-square-phone"></i> <span>+91-6367484603</span></p>
+                        <p><i className="fa-solid fa-envelope"></i> <span>piyushjain4855@gmail.com</span></p>
                         <p>Live customer care service available Mon-Sat 10 am - 7 pm on non-public holidays</p>
                     </div>
                     <div className="details">
                         Dilocious is your one-stop-shop if you seek and crave for local and regional flavours, unique products and authentically experience different cultures of entire India. At Dilocious, you can get limited available speciality food, a huge variety of tastes and offerings, regional variants of famous food and enjoy far distant seasonal delicacies right your doorstep.
                     </div>
                 </div>
-                <div id='copyright'> &copy; Ayush Gupta </div>
+                <div id='copyright'> &copy; Piyush Jain </div>
                 <Tippy content={'Back to top'} placement='left'>
                     <i className='fa-solid fa-circle-chevron-up' id='backToTop' style={{ display: visible ? 'inline' : 'none' }} onClick={scrollToTop}  ></i>
                 </Tippy>
